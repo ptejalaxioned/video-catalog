eb1e95a9b5948bba49b99dec5775e6d0d62207dc
ptejalaxioned <tejalp@axioned.com>
2024-10-03 14:24:28 +0530

    banner block code in content-banner commit

---
+++ wp-content/themes/video-catalog/inc/blocks/content-banner.php
@@ -0,0 +1,25 @@
+<?php
+$background_video = get_field('background_video');
+$banner_paragraph = get_field('banner_paragraph');
+$customize_link = get_field('customize_link');
+?>
+<?php if ($background_video || $banner_paragraph || $customize_link) { ?>
+  <!--banner section start-->
+  <section class="banner relative text-white">
+        <div class="wrapper w-full">
+          <figure>
+            <video src="<?php echo esc_url( $background_video ); ?>" title="Banner Video" autoplay loop muted>Your browser
+              does not support the video tag.</video>
+          </figure>
+          <div class="banner-content absolute z-30 transform -translate-x-1/2 -translate-y-1/2 bottom-1/4 left-1/2 w-5/12 flex flex-col gap-8">
+            <p class="single-caps text-2xl text-center leading-normal">
+            <?php echo $banner_paragraph ?>
+            </p>
+              <div class="banner-button flex justify-center">
+            <a href="<?php echo $customize_link ?>" target="_self" title="Discover" class="first-caps bg-black bg-opacity-45 px-14 py-4 rounded-md font-bold text-xl"><?php echo $customize_link ?></a>
+          </div>
+          </div>
+        </div>
+      </section>
+  <!--banner section end-->
+<?php } ?>
